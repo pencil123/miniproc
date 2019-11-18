e21462d67698200755389477879a071466c3d73e
张鲁燕 <zhangluyan@jsh.com>
2019-11-18 19:56:17 +0800

    add search page

--- pages/index/index.js
+++ pages/index/index.js
@@ -56,5 +56,25 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+
+  //预览图片
+  previewImg: function (e) {
+    var currentUrl = e.currentTarget.dataset.currenturl
+    var previewUrls = e.currentTarget.dataset.previewurl
+    wx.previewImage({
+      current: currentUrl, //必须是http图片，本地图片无效
+      urls: previewUrls, //必须是http图片，本地图片无效
+    })
+  },
+  onLoad: function () {
+    var that = this
+    var picList = []
+    picList.push("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1623318287,3864173199&fm=27&gp=0.jpg")
+    picList.push("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532429494100&di=6d4f20a64fb21f113e1bb67be1cdbb63&imgtype=0&src=http%3A%2F%2Fimg.juimg.com%2Ftuku%2Fyulantu%2F121019%2F240425-12101920154274.jpg")
+    picList.push("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532429494100&di=f3712ddf9ca5b37bf81f2cea4ae40c54&imgtype=0&src=http%3A%2F%2Fpic32.photophoto.cn%2F20140808%2F0042040230406581_b.jpg")
+    that.setData({
+      picList: picList,
+    })
   }
 })
