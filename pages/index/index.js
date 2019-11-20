dfbb6efa9a8d740ee951287f54dd0d0baa2e4823
张鲁燕 <zhangluyan@jsh.com>
2019-11-20 17:36:31 +0800

    add domain

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,44 +4,12 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '领取优惠券',
     userInfo: {},
     hasUserInfo: false,
     time: (new Date()).toString(),
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    array: [{
-      text: '高领秋冬韩版紧身内搭t恤打底',
-      price: '10.50',
-      src: 'https://img.alicdn.com/bao/uploaded/i1/3878483432/O1CN01gcgZHU1bDtqUBdIWp_!!0-item_pic.jpg'
-    }, {
-        text: '高领秋冬韩版紧身内搭t恤打底',
-        price: '10.50',
-        src: 'https://img.alicdn.com/bao/uploaded/i1/3878483432/O1CN01gcgZHU1bDtqUBdIWp_!!0-item_pic.jpg'
-      }, {
-        text: '高领秋冬韩版紧身内搭t恤打底',
-        price: '10.50',
-        src: 'https://img.alicdn.com/bao/uploaded/i1/3878483432/O1CN01gcgZHU1bDtqUBdIWp_!!0-item_pic.jpg'
-      }, {
-        text: '高领秋冬韩版紧身内搭t恤打底',
-        price: '10.50',
-        src: 'https://img.alicdn.com/bao/uploaded/i1/3878483432/O1CN01gcgZHU1bDtqUBdIWp_!!0-item_pic.jpg'
-      }, {
-        text: '高领秋冬韩版紧身内搭t恤打底',
-        price: '10.50',
-        src: 'https://img.alicdn.com/bao/uploaded/i1/3878483432/O1CN01gcgZHU1bDtqUBdIWp_!!0-item_pic.jpg'
-      }, {
-        text: '高领秋冬韩版紧身内搭t恤打底',
-        price: '10.50',
-        src: 'https://img.alicdn.com/bao/uploaded/i1/3878483432/O1CN01gcgZHU1bDtqUBdIWp_!!0-item_pic.jpg'
-      }, {
-        text: '高领秋冬韩版紧身内搭t恤打底',
-        price: '10.50',
-        src: 'https://img.alicdn.com/bao/uploaded/i1/3878483432/O1CN01gcgZHU1bDtqUBdIWp_!!0-item_pic.jpg'
-      }, {
-        text: '高领秋冬韩版紧身内搭t恤打底',
-        price: '10.50',
-        src: 'https://img.alicdn.com/bao/uploaded/i1/3878483432/O1CN01gcgZHU1bDtqUBdIWp_!!0-item_pic.jpg'
-      }],
+
   },
   //事件处理函数
   bindViewTap: function() {
@@ -54,43 +22,9 @@ Page({
       url:'../phone/phone'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
+  onLoad: function (options) {
 
+  },
   //预览图片
   previewImg: function (e) {
     var currentUrl = e.currentTarget.dataset.currenturl
@@ -108,6 +42,19 @@ Page({
     picList.push("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532429494100&di=f3712ddf9ca5b37bf81f2cea4ae40c54&imgtype=0&src=http%3A%2F%2Fpic32.photophoto.cn%2F20140808%2F0042040230406581_b.jpg")
     that.setData({
       picList: picList,
+    })
+    wx.request({
+      url: 'https://taobao71.com/welcome/wechat',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        that.setData(
+          {
+            list: res.data.data
+          }
+        )
+      }
     })
   }
 })
